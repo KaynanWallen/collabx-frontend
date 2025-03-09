9dc7e661382f86acc4fa8824c06f154b15405561
kaynanwallen <wallenkaynan@gmail.com>
2025-03-09 16:54:29 -0300

    feat: implemented component for  projects detail

--- app/components/explore/DetailCardProject.tsx
+++ app/components/explore/DetailCardProject.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {
+  ChevronUp,
   Github,
   MessageSquare,
   Minus,
@@ -19,6 +20,10 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "~/components/ui/drawer";
+import { ScrollArea } from "../ui/scroll-area";
+import { Badge } from "../ui/badge";
+import { Separator } from "../ui/separator";
+import { Textarea } from "../ui/textarea";
 
 interface DetailCardProjectProps {
   detailCardOpen: boolean;
@@ -32,62 +37,241 @@ export default function DetailCardProject({
   return (
     <>
       <Drawer open={detailCardOpen} onOpenChange={setDetailCardOpen}>
-        {/* <DrawerTrigger asChild>
-          <Button variant="outline">Open Drawer</Button>
-        </DrawerTrigger> */}
-        <DrawerContent className="max-h-[95vh] w-[95vw] mx-auto px-16 flex flex-col gap-4 overflow-y-auto">
-          <section className="W-full min-w-full h-full max-h-[728px] min-h-[728px] bg-[#DDDDDD] rounded-md"></section>
+        <DrawerContent className="max-h-[95vh] w-[95vw] h-[95vh] mx-auto gap-4">
+          <ScrollArea className="max-h-[95vh] w-[95vw] h-[95vh] mx-auto px-16 pt-10 flex flex-col gap-4">
+            <section className="W-full min-w-full h-full max-h-[728px] min-h-[728px] bg-[#DDDDDD] rounded-md"></section>
 
-          <section className="px-6">
-            <header className="flex flex-col gap-3 border-b pb-2">
-              <div className="flex flex-row justify-between items-center">
-                <span className="flex flex-row gap-3 items-center">
-                  <p className="text-4xl font-bold">Nome do projeto aqui</p>
-                  <Star className="text-[#F8C100] size-8" />
+            <section className="px-6 flex flex-col gap-5 mb-5">
+              <header className="flex flex-col gap-3 border-b pb-2">
+                <div className="flex flex-row justify-between items-center">
+                  <span className="flex flex-row gap-3 items-center">
+                    <p className="text-4xl font-bold">Nome do projeto aqui</p>
+                    <Star className="text-[#F8C100] size-8" />
+                  </span>
+
+                  <ul className="flex flex-row gap-2">
+                    <li className="flex flex-row items-center gap-2 font-semibold text-3xl">
+                      <ThumbsUp className="size-8 text-[#22C55E]" />
+                      12
+                    </li>
+
+                    <li className="flex flex-row items-center gap-2 font-semibold text-3xl">
+                      <ThumbsDown className="size-8 text-[#EF4444]" />3
+                    </li>
+
+                    <li className="flex flex-row items-center gap-2 font-semibold text-3xl">
+                      <MessageSquare className="size-8 text-[#3B82F6]" />
+                      18
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="flex flex-row justify-between items-center">
+                  <section className="flex flex-row gap-2">
+                    <div className="size-12 bg-pink-800 rounded-md flex items-center justify-center text-white">
+                      KW
+                    </div>
+                    <span>
+                      <p className="font-bold ">Kaynan Wallen</p>
+                      <p className="text-[#1E1E1E]/75 font-semibold">
+                        12/02/2024 13:22
+                      </p>
+                    </span>
+                  </section>
+
+                  <span className="flex flex-row gap-4">
+                    <Github className="text-muted-foreground size-7"/>
+                    <Github className="text-muted-foreground size-7"/>
+                    <Github className="text-muted-foreground size-7"/>
+                    <Github className="text-muted-foreground size-7"/>
+                  </span>
+                </div>
+              </header>
+
+              <article className="flex flex-col gap-5">
+                <span className="flex flex-col gap-3">
+                  <p className="text-4xl font-bold">Sobre o Projeto</p>
+                  <p className="text-xl">Um aplicativo para organizar sua rotina de autocuidado. Crie lembretes para skincare, hidratação e até mesmo aquela máscara facial que você sempre esquece de usar! O projeto foi desenvolvido utilizando React Native e Node.js, com foco em proporcionar uma experiência intuitiva e agradável aos usuários. A interface foi cuidadosamente projetada para ser minimalista e funcional, permitindo que os usuários se concentrem no que realmente importa: seu bem-estar.</p>
                 </span>
 
-                <ul className="flex flex-row gap-2">
-                  <li className="flex flex-row items-center gap-2 font-semibold text-3xl">
-                    <ThumbsUp className="size-8 text-[#22C55E]" />
-                    12
-                  </li>
+                <span className="flex flex-col gap-3">
+                  <p className="text-4xl font-bold">Tecnologias utilizadas</p>
+                  <div className="flex flex-row flex-wrap gap-2">
+                    <Badge variant={"secondary"} className="px-3 py-2 text-xl text-muted-foreground">
+                      Montes
+                    </Badge>
 
-                  <li className="flex flex-row items-center gap-2 font-semibold text-3xl">
-                    <ThumbsDown className="size-8 text-[#EF4444]" />3
-                  </li>
+                    <Badge variant={"secondary"} className="px-3 py-2 text-xl text-muted-foreground">
+                    Vulputate
+                    </Badge>
 
-                  <li className="flex flex-row items-center gap-2 font-semibold text-3xl">
-                    <MessageSquare className="size-8 text-[#3B82F6]" />
-                    18
+                    <Badge variant={"secondary"} className="px-3 py-2 text-xl text-muted-foreground">
+                      Montes
+                    </Badge>
+
+                    <Badge variant={"secondary"} className="px-3 py-2 text-xl text-muted-foreground">
+                    Lobortis
+                    </Badge>
+
+                    <Badge variant={"secondary"} className="px-3 py-2 text-xl text-muted-foreground">
+                    Placerat
+                    </Badge>
+                  </div>
+                </span>
+
+                <span className="flex flex-col gap-3">
+                  <p className="text-4xl font-bold">Imagens do Projeto</p>
+                  <div className="flex flex-row justify-center flex-wrap gap-5">
+                    <section className="w-[500px] h-[280px] bg-[#DDDDDD] rounded-md"></section>
+                    <section className="w-[500px] h-[280px] bg-[#DDDDDD] rounded-md"></section>
+                    <section className="w-[500px] h-[280px] bg-[#DDDDDD] rounded-md"></section>
+                  </div>
+                </span>
+
+                <Separator />
+              </article>
+
+              {/* Seçao de comentários pode ser movida para um componente a parte */}
+              <footer className="flex flex-col gap-5">
+                <span className="flex flex-row gap-3 items-center">
+                  <MessageSquare className="size-8"/>
+                  <p className="text-4xl font-bold"> Comentários </p>
+                </span>
+                
+                {/* Novo comentário */}
+                <div className="flex flex-row gap-3">
+                    <span className="size-12 bg-pink-800 rounded-md flex items-center justify-center text-white">
+                      KW
+                    </span>
+
+                    <section className="flex flex-col gap-3 w-full">
+                      <Textarea className="h-52"/>
+                      <span>
+                        <Button>
+                          Comentar
+                        </Button>
+                      </span>
+                    </section>
+                </div>
+                
+                {/* Comentários */}
+                <ul>
+                  <li className="flex flex-row gap-3 bg-[#F1F5F9] p-3 rounded-sm">
+                    <span className="size-12 bg-pink-800 rounded-md flex items-center justify-center text-white">
+                      KW
+                    </span>
+
+                    <section className="flex flex-col gap-3 w-full">
+                      <span className="flex flex-col gap-1">
+                        <p className="font-bold text-md">Kaynan Wallen</p>
+                        <p className="text-[#1E1E1E]/75 font-semibold text-sm">
+                          12/02/2024 13:22
+                        </p>
+                      </span>
+
+                      <span>
+                        <p className="text-md">Projeto muito interessante! Adorei a ideia.</p>
+                      </span>
+
+                      <span className="flex flex-row gap-5">
+                        <li className="flex flex-row items-center gap-2 font-semibold text-lg">
+                          <ThumbsUp className="size-5 text-[#22C55E]" />
+                          12
+                        </li>
+
+                        <li className="flex flex-row items-center gap-2 font-semibold text-lg">
+                          <ThumbsDown className="size-5 text-[#EF4444]" />3
+                        </li>
+
+                        <li className="flex flex-row items-center gap-2 font-semibold text-lg">
+                          <MessageSquare className="size-5 text-[#3B82F6]" />
+                          Responder
+                        </li>
+                      </span>
+                      
+                      {/* Sub comentários */}
+                      <ul className="flex flex-col gap-3">
+                        <li className="flex flex-row gap-3 bg-background p-3 rounded-sm">
+                          <span className="size-12 bg-pink-800 rounded-md flex items-center justify-center text-white">
+                            KW
+                          </span>
+
+                          <section className="flex flex-col gap-3 w-full">
+                            <span className="flex flex-col gap-1">
+                              <p className="font-bold text-md">Kaynan Wallen</p>
+                              <p className="text-[#1E1E1E]/75 font-semibold text-sm">
+                                12/02/2024 13:22
+                              </p>
+                            </span>
+
+                            <span>
+                              <p className="text-md">Projeto muito interessante! Adorei a ideia.</p>
+                            </span>
+
+                            <span className="flex flex-row gap-5">
+                              <li className="flex flex-row items-center gap-2 font-semibold text-lg">
+                                <ThumbsUp className="size-5 text-[#22C55E]" />
+                                12
+                              </li>
+
+                              <li className="flex flex-row items-center gap-2 font-semibold text-lg">
+                                <ThumbsDown className="size-5 text-[#EF4444]" />3
+                              </li>
+
+                              <li className="flex flex-row items-center gap-2 font-semibold text-lg">
+                                <MessageSquare className="size-5 text-[#3B82F6]" />
+                                Marcar
+                              </li>
+                            </span>
+                          </section>
+                        </li>
+
+                        <li className="flex flex-row gap-3 bg-background p-3 rounded-sm">
+                          <span className="size-12 bg-pink-800 rounded-md flex items-center justify-center text-white">
+                            KW
+                          </span>
+
+                          <section className="flex flex-col gap-3 w-full">
+                            <span className="flex flex-col gap-1">
+                              <p className="font-bold text-md">Kaynan Wallen</p>
+                              <p className="text-[#1E1E1E]/75 font-semibold text-sm">
+                                12/02/2024 13:22
+                              </p>
+                            </span>
+
+                            <span>
+                              <p className="text-md">Projeto muito interessante! Adorei a ideia.</p>
+                            </span>
+
+                            <span className="flex flex-row gap-5">
+                              <li className="flex flex-row items-center gap-2 font-semibold text-lg">
+                                <ThumbsUp className="size-5 text-[#22C55E]" />
+                                12
+                              </li>
+
+                              <li className="flex flex-row items-center gap-2 font-semibold text-lg">
+                                <ThumbsDown className="size-5 text-[#EF4444]" />3
+                              </li>
+
+                              <li className="flex flex-row items-center gap-2 font-semibold text-lg">
+                                <MessageSquare className="size-5 text-[#3B82F6]" />
+                                Marcar
+                              </li>
+                            </span>
+                          </section>
+                        </li>
+
+                        <li className="flex flex-row gap-1 items-center">
+                          <p className="font-semibold text-xl">Ver menos</p>
+                          <ChevronUp />
+                        </li>
+                      </ul>
+                    </section>
                   </li>
                 </ul>
-              </div>
-
-              <div className="flex flex-row justify-between items-center">
-                <section className="flex flex-row gap-2">
-                  <div className="size-12 bg-pink-800 rounded-md flex items-center justify-center text-white">
-                    KW
-                  </div>
-                  <span>
-                    <p className="font-bold ">Kaynan Wallen</p>
-                    <p className="text-[#1E1E1E]/75 font-semibold">
-                      12/02/2024 13:22
-                    </p>
-                  </span>
-                </section>
-
-                <span className="flex flex-row gap-4">
-                  <Github className="text-muted-foreground size-7"/>
-                  <Github className="text-muted-foreground size-7"/>
-                  <Github className="text-muted-foreground size-7"/>
-                  <Github className="text-muted-foreground size-7"/>
-                </span>
-              </div>
-            </header>
-          </section>
-
-          <section className="flex flex-col gap-2">
-          </section>
+              </footer>
+            </section>
+          </ScrollArea>
         </DrawerContent>
       </Drawer>
     </>
